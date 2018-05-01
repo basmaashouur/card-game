59db37125c611d392e6579400a7ae6368a9e3deb
Basma Ashour <basmaashouur@gmail.com>
2018-05-01 09:14:01 +0200

    Organize code

--- cards/main.cpp
+++ cards/main.cpp
@@ -1,5 +1,4 @@
 /*
-g++ main.o -o sfml-app -L<sfml-install-path>/lib -lsfml-graphics -lsfml-window -lsfml-system
 ./sfml-app
 */
 #include <SFML/Graphics.hpp>
@@ -7,25 +6,25 @@ g++ main.o -o sfml-app -L<sfml-install-path>/lib -lsfml-graphics -lsfml-window -
 using namespace std;
 using namespace sf;
 
+// Game Objects
+const sf::Color colorArray[6] = {Color::Cyan, Color::Blue, Color::Green, Color::Red,
+                                 Color::Yellow, Color::Magenta
+                                };
+vector<int>check(12, 0);
+vector<int>fin(12, 0);
+RenderWindow window(VideoMode(270, 450), "Cards Game");
+RectangleShape shape(Vector2f(50, 50));
+vector<pair<double, double > >allpos;
+double incx = 0.0, incy = 0.0;
+int numclick = 0, curvis = 0, found = 0;
+Event event;
 
-
-int main()
+// Function to Genrate shapes
+void gen(int ex, int wy)
 {
-    RenderWindow window(VideoMode(270, 450), "Cards Game");
-    RectangleShape shape(Vector2f(50, 50));
-    const sf::Color colorArray[6] = {Color::Cyan, Color::Blue, Color::Green, Color::Red,
-                                     Color::Yellow, Color::Magenta
-                                    };
-    int check[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    int fin [12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    vector<pair<double, double > >allpos;
-    double incx = 0.0, incy = 0.0;
-    int cn = 0, curvis = 0;
-
-
-    for (int x = 0; x < 4; x++)
+    for (int x = 0; x < ex; x++)
     {
-        for (int y = 0; y < 3; y++)
+        for (int y = 0; y < wy; y++)
         {
             allpos.push_back(make_pair(40.f + incx, 40.f + incy));
             incx += 70.0;
@@ -33,73 +32,85 @@ int main()
         incx = 0.0;
         incy += 70.0;
     }
+}
 
+// Function to draw shapes
+void drawShape()
+{
+    for (int i = 0; i < 12; i++)
+    {
+        shape.setPosition(allpos[i].first, allpos[i].second);
+        if (check[i] || fin[i])shape.setFillColor(colorArray[i % 6]);
+        else shape.setFillColor(Color::White);
+        window.draw(shape);
+
+    }
+}
+
+// Function to trace the cards when clicked on them
+void clickOnCard()
+{
+    double ex = event.mouseButton.x;
+    double wy = event.mouseButton.y;
+    for (int i = 0; i < 12; i++)
+    {
+        // the click is on card
+        if (ex >= allpos[i].first && ex <= allpos[i].first + 50 &&
+                wy >= allpos[i].second <= wy && wy <= allpos[i].second + 50)
+        {
+            shape.setPosition(allpos[i].first, allpos[i].second);
+            shape.setFillColor(colorArray[i % 6]);
+            check[i] = 1;
+            window.draw(shape);
+            numclick++;
+            curvis++;
+            // two cards clicked
+            if (curvis == 2)
+            {
+                curvis = 0;
+                for (int ii = 0; ii < 12; ii++)
+                {
+                    // check if the two cards are the same
+                    if (check[ii] && ii != i )
+                    {
+                        if (colorArray[i % 6] != colorArray[ii % 6])
+                            check[i] = 0, check[ii] = 0;
+                        else
+                            fin[i] = 1, fin[ii] = 1, found++;
+                    }
+                }
+            }
+            break;
+        }
+    }
+}
+
+
+void endGame()
+{
+
+}
+
+
+int main()
+{
+    gen(4, 3);
     while (window.isOpen())
     {
-        Event event;
+        window.clear();
+        drawShape();
+        window.display();
+        if (found == 6)endGame();
+
         while (window.pollEvent(event))
         {
             if (event.type == Event::Closed)
                 window.close();
 
-        // clicked on window
-            else if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left)
-            {
-                double ex = event.mouseButton.x;
-                double wy = event.mouseButton.y;
-                for (int i = 0; i < 12; i++)
-                {
-                    // the click is on card
-                    if (ex >= allpos[i].first && ex <= allpos[i].first + 50 &&
-                            wy >= allpos[i].second <= wy && wy <= allpos[i].second + 50)
-                    {
-                        shape.setPosition(allpos[i].first, allpos[i].second);
-                        shape.setFillColor(colorArray[i % 6]);
-                        check[i] = 1;
-                        window.draw(shape);
-                        cn++;
-                        curvis++;
-                        // two cards clicked
-                        if (curvis == 2)
-                        {
-                            curvis = 0;
-                            for (int ii = 0; ii < 12; ii++)
-                            {
-                                // check if the two cars are the same
-                                if (check[ii] && ii != i )
-                                {
-                                    if (colorArray[i % 6] != colorArray[ii % 6])
-                                        check[i] = 0, check[ii] = 0;
-                                    else
-                                        fin[i] = 1, fin[ii] = 1;
-                                }
-                            }
-                        }
-                        break;
-                    }
-                }
-            }
+            else if (event.type == sf::Event::MouseButtonPressed &&
+                     event.mouseButton.button == Mouse::Left)
+                clickOnCard();
         }
-        window.clear();
-        // display shapes on the window
-        for (int i = 0; i < 12; i++)
-        {
-            shape.setPosition(allpos[i].first, allpos[i].second);
-            if (check[i] || fin[i])shape.setFillColor(colorArray[i % 6]);
-            else shape.setFillColor(Color::White);
-            window.draw(shape);
-
-        }
-
-        // game End
-        if (cn == 12)
-        {
-
-
-        }
-
-        window.display();
-
     }
 
     return 0;
